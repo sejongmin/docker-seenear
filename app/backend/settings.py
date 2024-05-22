76600a9d643f876067c67e7b38e5f5a92f323315
OhJongMin <jmoh7480@gmail.com>
2024-05-23 00:02:42 +0900

    feat: edit cors

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 REST_FRAMEWORK = {
